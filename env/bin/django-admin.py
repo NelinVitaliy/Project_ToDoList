a831b94c6205947e07f8b839b3af106ff3d13b63
NelinVitaliy <s5001481.nelin@gmail.com>
2019-12-28 15:16:47 +0600

    ToDoList Project

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nelin/NeLiN/MakerS Python/TASKS/Chapter_Part_6/myproject/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
